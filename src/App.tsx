1dbbb449786135ab2617cf73fc3843f81be0c656
Juan Gómez <gomezcabo@gmail.com>
2025-02-28 16:05:19 +0000

    mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { Vex } from "vexflow";
+
+import { clsx } from "clsx";
 
 type Note = {
   name: string;
@@ -7,6 +9,7 @@ type Note = {
 };
 
 const NOTES: Note[] = [
+  // { name: "Si", key: "b/3" },
   { name: "Do", key: "c/4" },
   { name: "Re", key: "d/4" },
   { name: "Mi", key: "e/4" },
@@ -20,13 +23,13 @@ const NOTES: Note[] = [
   { name: "Fa", key: "f/5" },
   { name: "Sol", key: "g/5" },
   { name: "La", key: "a/5" },
-  { name: "Si", key: "b/4" },
+  { name: "Si", key: "b/5" },
+  { name: "Do", key: "c/6" },
 ];
 
 export function App() {
   const [currentNote, setCurrentNote] = useState<Note>(NOTES[0]);
-  const [feedback, setFeedback] = useState("Correcto");
-  const [score, setScore] = useState(0);
+  const [feedback, setFeedback] = useState("");
   const staffRef = useRef<HTMLDivElement>(null!);
 
   const renderStaff = useCallback(() => {
@@ -44,7 +47,7 @@ export function App() {
     context.scale(3, 3);
 
     // Create a stave
-    const stave = new VF.Stave(13, -13, 100);
+    const stave = new VF.Stave(13, -10, 100);
     stave.addClef("treble");
     stave.setContext(context).draw();
 
@@ -52,7 +55,7 @@ export function App() {
     const note = new VF.StaveNote({
       clef: "treble",
       keys: [currentNote.key],
-      duration: "q",
+      duration: "w",
     });
 
     // Get the line number for the note (0 is the bottom line, 6 is the top line)
@@ -66,7 +69,7 @@ export function App() {
     }
 
     // Create a voice and add the note
-    const voice = new VF.Voice({ num_beats: 1, beat_value: 4 });
+    const voice = new VF.Voice({ num_beats: 1, beat_value: 1 });
     voice.addTickables([note]);
 
     // Format and draw the voice
@@ -85,34 +88,57 @@ export function App() {
   }, [currentNote, staffRef, renderStaff]);
 
   const generateNewNote = () => {
-    const randomIndex = Math.floor(Math.random() * NOTES.length);
-    setCurrentNote(NOTES[randomIndex]);
-    setFeedback("Correcto");
+    let randomIndex = Math.floor(Math.random() * NOTES.length);
+    let newNote = NOTES[randomIndex];
+    while (newNote === currentNote) {
+      randomIndex = Math.floor(Math.random() * NOTES.length);
+      newNote = NOTES[randomIndex];
+    }
+    setCurrentNote(newNote);
+    setFeedback("");
   };
 
   const handleNoteClick = (selectedNote: Note) => {
     if (selectedNote.name === currentNote.name) {
       setFeedback("¡Correcto! Es un " + currentNote.name.toUpperCase());
-      setScore(score + 1);
       setTimeout(generateNewNote, 1500);
     } else {
-      setFeedback(`Incorrecto: ${currentNote.name.toUpperCase()}`);
+      setFeedback(`¡Incorrecto! Es un ${currentNote.name.toUpperCase()}`);
       setTimeout(generateNewNote, 2500);
     }
   };
 
+  const candidateNotes = useMemo(() => {
+    const newNotes = [...NOTES]
+      .slice(0, 7)
+      .sort(() => Math.random() - 0.5)
+      .filter((note) => note.name !== currentNote.name)
+      .slice(0, 3)
+      .concat(currentNote)
+      .sort(() => Math.random() - 0.5);
+
+    return newNotes;
+  }, [currentNote]);
+
   return (
-    <div className="min-h-screen min-w-[640px] w-full flex flex-col items-center justify-center p-4 bg-gradient-to-b from-teal-100 to-teal-100">
-      <div className="w-full max-w-4xl bg-white rounded-2xl shadow-xl p-8 mx-auto">
+    <div
+      className={clsx({
+        "h-screen min-w-[480px] w-full flex flex-col items-center justify-center p-8 bg-gradient-to-b": true,
+        "from-green-400 to-green-300": feedback.includes("Correcto"),
+        "from-red-600 to-red-400": feedback.includes("Incorrecto"),
+        "from-pink-400 to-teal-300": feedback === "",
+      })}
+    >
+      <div className="w-full h-full md:h-auto relative max-w-4xl bg-white rounded-2xl shadow-xl md:py-16 py-32 p-8 mx-auto">
         <div className="flex justify-center">
           <div ref={staffRef} className="staff-container"></div>
         </div>
 
-        <div className="h-16 my-2 text-center">
+        <div className="absolute top-0 left-0 w-full">
           {feedback && (
             <div
-              className={`inline-block p-4 rounded-xl text-center text-lg font-semibold ${
-                feedback.includes("Correcto") ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"
+              className={`p-6 rounded-tl-xl rounded-tr-xl text-center text-2xl font-semibold ${
+                feedback.includes("Correcto") ? "text-green-800" : "text-red-800"
               }`}
             >
               {feedback}
@@ -120,18 +146,20 @@ export function App() {
           )}
         </div>
 
-        <div className="max-w-2xl mx-auto">
-          <p className="text-2xl font-medium text-teal-800 mb-6 text-center">¿Qué nota es esta?</p>
-          <div className="grid grid-cols-4 md:grid-cols-7 gap-4">
-            {NOTES.slice(0, 7).map((note) => (
+        <div className="max-w-md mx-auto">
+          <p className="text-2xl font-semibold text-black mt-6 mb-6 text-center">¿Qué nota es esta?</p>
+          <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+            {candidateNotes.map((note) => (
               <button
+                disabled={feedback !== ""}
                 key={note.name}
                 onClick={() => handleNoteClick(note)}
-                className="cursor-pointer bg-gradient-to-r from-teal-500 to-teal-600 hover:from-teal-400 hover:to-teal-500 
-                text-white font-bold py-4 px-6 rounded-xl shadow-md hover:shadow-lg 
-                transform hover:scale-105 transition-all duration-200 text-lg"
+                className={clsx(
+                  "cursor-pointer bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-4 px-6 rounded-xl shadow-md transform hover:scale-105 transition-all duration-200 text-2xl",
+                  feedback !== "" && "disabled:opacity-50 pointer-events-none"
+                )}
               >
-                {note.name}
+                {note.name.toUpperCase()}
               </button>
             ))}
           </div>
